640850998ec8df4f6e61cae64130a34655cb4d0c
mozhu621 <mozhu621@gmail.com>
2024-08-06 17:43:25 +0800

    213

--- Evalution/run_3.sh
+++ Evalution/run_3.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # 定义 inference.py 需要的参数
-MODEL_TYPE="Qwen/Qwen2-7B-Instruct"
+MODEL_TYPE="google/gemma-2-9b"
 MODEL_NAME=$(basename $MODEL_TYPE)
 MAX_LENGTH=16000
-NUM_GPUS=2
+NUM_GPUS=8
 OUTPUT_DIR="./results"
 OUTPUT_FILE="${OUTPUT_DIR}/${MODEL_NAME}_maxlen${MAX_LENGTH}.json"
-export CUDA_VISIBLE_DEVICES=0,1
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 # 确保输出目录存在
 
 
