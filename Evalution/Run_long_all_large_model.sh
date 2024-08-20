dff6d84e924262eafdb0e9f03890a39fb9c97cc0
mozhu621 <mozhu621@gmail.com>
2024-08-20 13:44:20 +0800

    12

--- Evalution/Run_long_all_large_model.sh
+++ Evalution/Run_long_all_large_model.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # 定义 inference.py 需要的参数
-MODEL_TYPE="mistralai/Mistral-7B-Instruct-v0.2"
-# MODEL_TYPE="Qwen/Qwen2-7B-Instruct"
+#MODEL_TYPE="mistralai/Mistral-7B-Instruct-v0.2"
+MODEL_TYPE="Qwen/Qwen2-72B-Instruct"
 # MODEL_TYPE="meta-llama/Meta-Llama-3.1-8B-Instruct"
 # MODEL_TYPE="google/gemma-2-9b"
 # MODEL_TYPE="In2Training/FILM-7B"
 # export VLLM_ATTENTION_BACKEND=FLASHINFER
 
 
-
+MODEL_TYPE="Qwen/Qwen2-72B-Instruct"
 MODEL_NAME=$(basename $MODEL_TYPE)
-MAX_LENGTH=32000
+MAX_LENGTH=16000
 NUM_GPUS=8
 INPUT_DIR="/home/yuhao/THREADING-THE-NEEDLE/Dataset/Dataset_long.json"
 OUTPUT_DIR="./results"
@@ -26,7 +26,7 @@ python eval.py --data $OUTPUT_FILE --csv $CSV_PATH --gpu $NUM_GPUS
 
 
 
-MODEL_TYPE="Qwen/Qwen2-7B-Instruct"
+MODEL_TYPE="meta-llama/Meta-Llama-3.1-70B-Instruct"
 MODEL_NAME=$(basename $MODEL_TYPE)
 OUTPUT_FILE="${OUTPUT_DIR}/${MODEL_NAME}_maxlen${MAX_LENGTH}.json"
 
@@ -35,7 +35,7 @@ python inference.py --model $MODEL_TYPE --max_length $MAX_LENGTH --gpu $NUM_GPUS
 python eval.py --data $OUTPUT_FILE --csv $CSV_PATH --gpu $NUM_GPUS
 
 
-MODEL_TYPE="meta-llama/Meta-Llama-3.1-8B-Instruct"
+MODEL_TYPE="google/gemma-2-27b"
 MODEL_NAME=$(basename $MODEL_TYPE)
 OUTPUT_FILE="${OUTPUT_DIR}/${MODEL_NAME}_maxlen${MAX_LENGTH}.json"
 
@@ -44,7 +44,7 @@ python inference.py --model $MODEL_TYPE --max_length $MAX_LENGTH --gpu $NUM_GPUS
 python eval.py --data $OUTPUT_FILE --csv $CSV_PATH --gpu $NUM_GPUS
 
 
-MODEL_TYPE="google/gemma-2-9b"
+MODEL_TYPE="mistralai/Mixtral-8x7B-Instruct-v0.1"
 MODEL_NAME=$(basename $MODEL_TYPE)
 OUTPUT_FILE="${OUTPUT_DIR}/${MODEL_NAME}_maxlen${MAX_LENGTH}.json"
 
@@ -52,10 +52,3 @@ python inference.py --model $MODEL_TYPE --max_length $MAX_LENGTH --gpu $NUM_GPUS
 # 运行 eval.py
 python eval.py --data $OUTPUT_FILE --csv $CSV_PATH --gpu $NUM_GPUS
 
-
-MODEL_TYPE="In2Training/FILM-7B"
-MODEL_NAME=$(basename $MODEL_TYPE)
-OUTPUT_FILE="${OUTPUT_DIR}/${MODEL_NAME}_maxlen${MAX_LENGTH}.json"
-python inference.py --model $MODEL_TYPE --max_length $MAX_LENGTH --gpu $NUM_GPUS  --input_file $INPUT_DIR  --output_file $OUTPUT_FILE 
-# 运行 eval.py
-python eval.py --data $OUTPUT_FILE --csv $CSV_PATH --gpu $NUM_GPUS
