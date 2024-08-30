e0594e653f9817c4ccc628f9810588e7b434d9cd
mozhu621 <mozhu621@gmail.com>
2024-08-30 14:49:04 +0800

    412341

--- Evalution/run_4.sh
+++ Evalution/run_4.sh
@@ -16,13 +16,13 @@ MAX_LENGTH=16000
 NUM_GPUS=2 
 INPUT_DIR="/home/yuhao/THREADING-THE-NEEDLE/Dataset/Dataset_short.json"
 OUTPUT_DIR="./results"
-OUTPUT_FILE="${OUTPUT_DIR}/${MODEL_NAME}_maxlen${MAX_LENGTH}.json"
-export CUDA_VISIBLE_DEVICES=0,1
+OUTPUT_FILE="/home/yuhao/THREADING-THE-NEEDLE/Evalution/results/Qwen2-72B-Instruct_maxlen32000.json"
+export CUDA_VISIBLE_DEVICES=6,7
 # 确保输出目录存在
 
 
 # 运行 inference.py
-python inference.py --model $MODEL_TYPE --max_length $MAX_LENGTH --gpu $NUM_GPUS  --input_file $INPUT_DIR  --output_file $OUTPUT_FILE 
+# python inference.py --model $MODEL_TYPE --max_length $MAX_LENGTH --gpu $NUM_GPUS  --input_file $INPUT_DIR  --output_file $OUTPUT_FILE 
 
 CSV_PATH="/home/yuhao/THREADING-THE-NEEDLE/Evalution/results/accuracy_results.csv"
 
